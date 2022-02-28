14637b1f0a7f930d230fcb8fcd807251beada7a8
Jeremie <jeremiedevelops@gmail.com>
2022-02-28 12:02:12 -0500

    updated styling

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import json
 
 # this is for getting the secret key
 with open('secrets.json') as f:
-  data = json.load(f)
+    data = json.load(f)
 
 # creating an instance of Flask as our app
 app = Flask(__name__)
@@ -17,26 +17,27 @@ db = SQLAlchemy(app)
 # create model for item
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(256), nullable=False)
+    name = db.Column(db.String(256), nullable=False)
 
     def __repr__(self):
-        return f'Item {self.id} => {self.content}'
+        return f'Item {self.id} => {self.name}'
 
 # home
 @app.route('/', methods=['GET'])
-@app.route('/home', methods=['GET'])
 def home():
     return render_template('home.html')
 
 # add item
 @app.route('/add', methods=['GET', 'POST'])
 def add():
+    # if a form is submitted
     if request.method == 'POST':
-        content = request.form['content']
-        item = Item(content=content)
+        name = request.form['name']
+        item = Item(name=name)
         db.session.add(item)
         db.session.commit()
-        return redirect(url_for('home'))
+        return redirect(url_for('add'))
+    # if a user is going to the page
     else:
         return render_template('add.html')
 
@@ -52,21 +53,25 @@ def delete(id):
     item = Item.query.get_or_404(id)
     db.session.delete(item)
     db.session.commit()
-    return redirect(url_for('home'))
+    return redirect(url_for('view'))
 
 # update item
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     item = Item.query.get_or_404(id)
+
+    # if a form is submitted
     if request.method == 'POST':
-        item.content = request.form['content']
+        item.name = request.form['name']
         db.session.commit()
-        return redirect(url_for('home'))
+        return redirect(url_for('view'))
+    # if a user is going to the page
     else:
         return render_template('update.html', item=item)
 
 # this is what allows you to run the app
 if __name__ == "__main__":
+
     # creating db if it doesn't exist
     if not os.path.exists('site.db'):
         db.create_all()
